afa000f334882126dd1579985e11ed2330387b6f
gabby-m056 <144246046+gabby-m056@users.noreply.github.com>
2025-10-23 16:45:21 +0100

    save

--- Assets/FPSCollision.cs
+++ Assets/FPSCollision.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FPSCollision : MonoBehaviour
 {
 
-    public int playerAmmo =0
+    public int playerAmmo =0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
