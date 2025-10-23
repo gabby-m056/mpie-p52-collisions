c6d5a4626f0947943c2da82f9550223deb73a464
gabby-m056 <144246046+gabby-m056@users.noreply.github.com>
2025-10-23 16:44:27 +0100

    save

---
+++ Assets/FPSCollision.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class FPSCollision : MonoBehaviour
+{
+
+    public int playerAmmo =0
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        
+        if(other.gameObject.name == "AmmoBox"){
+            playerAmmo +=20
+            other.gameObject.SetActive(false);
+        }
+    }
+}
